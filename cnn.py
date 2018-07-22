eb7a05e8371e70dd67beecfd99aec7c34ce6b69d
Vadim Nazarov <vdn@mailbox.org>
2018-07-22 18:28:22 +0300

    minor fix

--- cnn.py
+++ cnn.py
@@ -22,8 +22,8 @@ import torch.nn.functional as F
 from torch import optim
 
 
-__all__ = ['ResNet', 'resnet18', 'resnet34', 'resnet50', 'resnet101',
-           'resnet152']
+# __all__ = ['ResNet', 'resnet18', 'resnet34', 'resnet50', 'resnet101',
+#            'resnet152']
 
 
 model_urls = {
